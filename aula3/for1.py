3e6e9c1db57283ca4ad484db763eb3f4d6c74406
Carlos Freitas <carlosfreitassdev@gmail.com>
2026-04-12 16:04:25 -0300

    Corrigindo comentários em exemplo de laço de repetição.

--- aula3/for1.py
+++ aula3/for1.py
@@ -20,6 +20,6 @@ for i in range(0, -4, -1):
   print(i, end=' ')
 print('\n')
 
-# Range(10) -> 1, 2, 3, 4, 5, 6, 7, 8, 9
+# Range(10) -> 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 for i in range(10):
   print(i, end=' ')
